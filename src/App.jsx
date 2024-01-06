d3690865e62e548e14fef3d9f07b3f5830baab16
Emmanuel Balogun <emmanx25@gmail.com>
2024-01-06 04:18:33 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+// Importing necessary dependencies from React
 import { useState, useEffect } from "react";
 import ImageCards from "./Components/ImageCards";
 import ImageSearch from "./Components/ImageSearch";
 import github from "./assets/github.png"
 
+// Main App component
 function App() {
+  // State variables for managing images, loading state, and search term
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("");
 
+  // useEffect to fetch images from Pixabay API when the search term changes
   useEffect(() => {
+    // Using setTimeout to simulate a loading delay for better user experience
     setTimeout(() => {
       fetch(
         `https://pixabay.com/api/?key=41448682-600be54c66c121fc6ad309ce1&q=${term}&image_type=photo&pretty=true`
@@ -22,24 +27,33 @@ function App() {
     }, 1000);
   }, [term]);
 
+  // Render the main application UI
   return (
     <div>
+      {/* Navigation bar */}
       <nav className="p-6 flex items-center justify-between">
         <div className="">
           <h1 className="font-bold font-mono text-xl">SearchImg</h1>
         </div>
         <div className="github">
+          {/* GitHub link */}
           <a href="https://github.com/Devemmy01/ImageGallery">
             <img src={github} alt="" />
           </a>
         </div>
       </nav>
+
+      {/* Image search component */}
       <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {/* Container for displaying images */}
       <div className="container mb-20 mt-36 mx-auto transition-all">
+        {/* Display a message if no images are available */}
         {!isLoading && images.length === 0 && (
-          <h1 className="text-3xl font-bold text-gray-600 md:text-5xl font-mono text-center mx-auto mt-44 md:mt-32">No result found</h1>
+          <h1 text-5xl font-mono text-center mx-auto mt-32></h1>
         )}
 
+        {/* Loading animation while images are being fetched */}
         {isLoading ? (
           <div className="flex items-center justify-center gap-3">
             <span className="text-6xl animate-bounce">.</span>
@@ -47,6 +61,7 @@ function App() {
             <span className="text-6xl animate-bounce">.</span>
           </div>
         ) : (
+          // Display the grid of image cards once images are loaded
           <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-full gap-4">
             {images.map((image) => (
               <ImageCards key={image.id} image={image} />
@@ -58,4 +73,5 @@ function App() {
   );
 }
 
+// Exporting the App component as the default export
 export default App;
